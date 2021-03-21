89f7713d9ceb3ca990a0285641d03924d781a5eb
omidiora <omidioraemmanuel@gmail.com>
2021-03-21 20:07:37 +0100

    initial commit

--- App.js
+++ App.js
@@ -1,12 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View ,FlatList ,TouchableOpacity } from 'react-native';
+import  Header  from './component/Header';
+import TodoItem from './component/TodoItem';
 
 export default function App() {
+
+  const [todos , setTodos] =useState([
+    {text:'Buy Coffee' , key:'1'} , 
+    {text:'Create an app' , key:'2'} , 
+    {text:'Play on the swithc' , key:'3'} , 
+  ]);
+
+  const pressHandler=(key)=>{
+    setTodos((prevTodos)=>{
+      return prevTodos.filter(todo=>todo.key !=key);
+    })
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    
+    {/* header */}
+    <Header/>
+    
+    <View style={styles.content}>
+    <View style={styles.list} >
+      <FlatList data={todos}   renderItem={(({item})=>(
+
+       <TodoItem item={item}  pressHandler={pressHandler}/> 
+      ))} >
+
+      </FlatList>
+
+    </View>
+      
+    </View>
+      
+
+     
     </View>
   );
 }
@@ -15,7 +47,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  
   },
+  content:{
+    padding:40
+  },
+  list:{
+    marginTop:20
+
+  }
+
+
 });
